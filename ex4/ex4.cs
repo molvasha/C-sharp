a6321324ec701055f5a9f716399c852fcb82acd3
molvasha <molvasha@gmail.com>
2023-08-05 15:41:45 +0300

    ex4

---
+++ ex4/ex4.cs
@@ -0,0 +1,8 @@
+﻿// вывести последний символ трехзначного числа
+
+/*string num = Console.ReadLine();
+System.Console.WriteLine(num[2]);*/ //неправильное решение
+
+int num = int.Parse(Console.ReadLine());
+int lastSymb = num % 10;
+Console.WriteLine(lastSymb);
