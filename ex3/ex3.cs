7c1c7d1a3e4f7ddb1ad7263577556234b9f1ff2b
molvasha <molvasha@gmail.com>
2023-08-05 15:25:15 +0300

    ex3.2

--- ex3/ex3.cs
+++ ex3/ex3.cs
@@ -4,36 +4,43 @@ int num1 = int.Parse(Console.ReadLine());
 if (num1 == 1)
 {
     Console.WriteLine("понедельник");
+    return;
 }
 
 if (num1 == 2)
 {
     Console.WriteLine("вторник");
+    return;
 }
 
 if (num1 == 3)
 {
     Console.WriteLine("среда");
+    return;
 }
 
 if (num1 == 4)
 {
     Console.WriteLine("четверг");
+    return;
 }
 
 if (num1 == 5)
 {
     Console.WriteLine("пятница");
+    return;
 }
 
 if (num1 == 6)
 {
     Console.WriteLine("суббота");
+    return;
 }
 
 if (num1 == 7)
 {
     Console.WriteLine("воскресенье");
+    return;
 }
 
 else 
