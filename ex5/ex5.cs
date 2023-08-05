408a6532c0db1ea2f26d192eef896bc0ea958d4e
molvasha <molvasha@gmail.com>
2023-08-05 15:48:34 +0300

    ex5

---
+++ ex5/ex5.cs
@@ -0,0 +1,8 @@
+﻿// вывести числа в промежутке от n до -n
+int num = int.Parse(Console.ReadLine());
+int otrNum = -num;
+while (otrNum <= num)
+{
+    System.Console.WriteLine(otrNum);
+    otrNum++;
+}
